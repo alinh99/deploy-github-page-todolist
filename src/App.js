e4a18a0460aeb2151bc4a7aff61e835a34ace9a6
unknown <alinh1803@gmail.com>
2021-01-04 00:54:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import AccountAction from './containers/AccountAction/AccountAction';
-import { HashRouter, Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
-import { createBrowserHistory } from "history";
+
 import history from './history/history';
 
 function App() {
